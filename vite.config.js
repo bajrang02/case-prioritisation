4c206bb36b3bda756e0dad35ddc4330a7af83a32
Bajrang Lal <86866869+bajrang02@users.noreply.github.com>
2026-03-25 20:33:17 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     open: true,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://case-prioritisation-1.onrender.com',
         changeOrigin: true
       }
     }
